315b7cee4060187500e1d501d81a220b68bb8d32
Micah Rassi <mrassi@polywood.com>
2021-06-18 11:50:57 -0400

    IMAGES WEEEEE

--- quote/assets/js/loader.js
+++ quote/assets/js/loader.js
@@ -1,7 +1,9 @@
 var quote = document.getElementById("quote");
+var img = document.getElementById("bg");
 
 var quoteArray = [];
 var index = 1;
+var quoteText = "";
 
 function readData(json) {
     var data = json.feed.entry;
@@ -20,7 +22,15 @@ function nextQuote(){
 }
 
 function quoteReplace(){
-    quote.innerHTML = quoteArray[index];
+    qouteText = quoteArray[index];
+    if(validURL(qouteText)){
+        var imgStr = "URL(" + qouteText + ")";
+        quote.style.content = imgStr;
+        quote.innerHTML = '';
+    }else{
+        quote.style.content = '';
+        quote.innerHTML = qouteText;
+    }
     index ++;
     if(index >= quoteArray.length){
         index = 0;
@@ -34,3 +44,13 @@ function shuffleArray(array) {
         [array[i], array[j]] = [array[j], array[i]];
     }
 }
+
+function validURL(str) {
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    return !!pattern.test(str);
+  }
